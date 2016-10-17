112eb677f3ed824174d9bff18d6755404810aeb0
RaviTeja <raviteja7.talari@gmail.com>
2016-10-18 00:27:23 +0530

    creation successfully (partial ga) completed ra

--- app/src/main/java/com/example/raviteja/crudjson/FileCreation.java
+++ app/src/main/java/com/example/raviteja/crudjson/FileCreation.java
@@ -1,39 +1,146 @@
 package com.example.raviteja.crudjson;
 
+import android.content.Context;
 import android.os.Environment;
+import android.util.Log;
+import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by RaviTeja on 11-10-2016.
  */
 public class FileCreation {
+    JSONObject mO = null;
+    JSONObject jO = new JSONObject();
+    int flag=0;
+    Context c;
+    List<String> textArray;
 
-    public void fileCreationCall(JSONObject obj)
+    public void fileCreationCall(Context ctx, String name, String h_name)
     {
+        c=ctx;
         File root = new File(Environment.getExternalStorageDirectory().getAbsolutePath());
+        String previousJson = null;
         if(!root.exists())
         {
             root.mkdirs();
         }
-        File file = new File(root, "avinash1.txt");
-        try {
-            if (file.exists()) {file.delete ();}
-            FileWriter fw = new FileWriter(file);
-            BufferedWriter bw = new BufferedWriter(fw);
-            bw.write(obj.toString());
-            bw.close();
+        File file = new File(root, "avinash1.json");
+        if(file.exists())
+        {
+            previousJson= fileReader(file);
+            Log.e("in if condition",previousJson);
+        }
+        else {
+            previousJson = "{}";
+        }
+        try
+        {
+                mO = new JSONObject(previousJson);
+                jO.put("name", name);
+                jO.put("hostel", h_name);
+                mO.accumulate("id", jO);
+                //checkUnique(mO);
+                Log.e("after", mO.toString());
+                String jsonString = null;
+                jsonString = mO.toString(4);
+                Log.e("final result",jsonString);
+        //        deletionJSON(mO,textArray,name);
+                fileWriter(mO,file);
+        }
+        catch (JSONException e)
+        {
+            e.printStackTrace();
+        }
+    }
+    public String fileReader(File f)
+    {
+        StringBuilder text = new StringBuilder();
 
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(f));
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                text.append(line);
+                text.append('\n');
+            }
+            br.close();
+        }
+        catch (IOException e) {
+            //You'll need to add proper error handling here
+        }
+        Log.e("wtffff",text.toString());
+        return  text.toString();
+    }
+    public void fileWriter(JSONObject json,File f)
+    {
+        try {
+            FileWriter fw = new FileWriter(f);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(json.toString());
+            bw.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+    public void checkUnique(JSONObject json)
+    {
+        textArray = new ArrayList<String>();
+        try {
+            JSONArray json_array= json.getJSONArray("id");
+            for(int i=0;i<json_array.length();i++)
+            {
+                String checkValue = json_array.getJSONObject(i).getString("name");
+                textArray.add(checkValue);
+            }
+            Log.e("arraylist",textArray.toString());
+        }
+        catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+    public void deletionJSON(JSONObject json,List<String> list,String name)
+    {
+        int x=0;
+        try {
+            JSONArray arr=json.getJSONArray("id");
+            for (int i=0; i<list.size(); i++)
+            {
+                if (list.get(i).equals(name))
+                {
+                    x++;
+                    if (x==2)
+                    {
+                        arr.remove(i);
+                        Log.e("removed finally","at index "+i);
+                        x=0;
+                    }
+                    else
+                    {
+                        x--;
+                    }
+                }
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
     }
 }
--- app/src/main/java/com/example/raviteja/crudjson/JsonCreation.java
+++ app/src/main/java/com/example/raviteja/crudjson/JsonCreation.java
@@ -7,14 +7,19 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class JsonCreation extends AppCompatActivity {
 
     EditText eName,eHostel;
     public String n,h;
     FileCreation fileObj;
+    List<String> textArray = new ArrayList<String>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,18 +34,34 @@ public class JsonCreation extends AppCompatActivity {
         eHostel= (EditText) findViewById(R.id.editText_hostel);
         n= eName.getText().toString();
         h= eHostel.getText().toString();
-
-
-        JSONObject mainObj = new JSONObject();
-        try {
-            mainObj.put("name",n);
-            mainObj.put("hostel",h);
-            Toast.makeText(getApplicationContext(),eName.getText()+"  "+eHostel.getText(),Toast.LENGTH_SHORT).show();
-        } catch (JSONException e) {
-            e.printStackTrace();
+        int flag=1;
+        int s = textArray.size();
+        if (textArray.size()==0)
+        {
+            textArray.add(n);
+            fileObj.fileCreationCall(getApplicationContext(),n,h);
         }
-        Log.e("error",n+"   "+h);
-        fileObj.fileCreationCall(mainObj);
-        Toast.makeText(getApplicationContext(),"check ES explorer",Toast.LENGTH_SHORT).show();
+        else
+        {
+            for (int i=0; i< textArray.size();i++)
+            {
+                if (textArray.get(i).equals(n))
+                {
+                    flag++;
+                    Toast.makeText(getApplicationContext(),"redundancy",Toast.LENGTH_SHORT).show();
+                    break;
+                }
+            }
+            if (flag==1)
+            {
+                textArray.add(n);
+                fileObj.fileCreationCall(getApplicationContext(),n,h);
+                Toast.makeText(getApplicationContext(),"check ES explorer",Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+    public void onTesting(View v)
+    {
+
     }
 }
